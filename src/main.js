139dc577e257a56a8398acf78d124bcc34adc734
Bashar <bashikr@gmail.com>
2022-03-20 15:38:19 +0100

    Add fontawesome global configurations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+library.add(fas);
+library.add(fab);
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
