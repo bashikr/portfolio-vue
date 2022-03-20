eb7b8e057a199630c15fbf2452d28699dac09355
Bashar <bashikr@gmail.com>
2022-03-20 11:22:06 +0100

    Add layout component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Layout />
 </template>
 
+<script>
+import Layout from "./components/Layout.vue";
+
+export default {
+  name: "App",
+  components: {
+    Layout,
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -16,6 +19,6 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #E0E0E6;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content py-4 h-screen flex flex-row content-start">
+    <div class="basis-1/5 mr-8">
+      <LeftSideBar />
+    </div>
+    <div class="basis-3/4 mr-8">
+      <Main />
+    </div>
+    <div class="basis-0.5">
+      <RightSideBar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Main from "./Main.vue";
+import LeftSideBar from "./LeftSidebar/Wrapper.vue";
+import RightSideBar from "./RightSideBar.vue";
+
+export default {
+  name: "Layout",
+  components: {
+    Main,
+    LeftSideBar,
+    RightSideBar,
+  },
+};
+</script>
+
+<style>
+.content {
+  max-width: 93.3333%;
+  margin: auto;
+}
+</style>
