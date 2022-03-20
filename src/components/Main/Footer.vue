91c3090f64985cf9ea40e6a844f3fbf98046a2bd
Bashar <bashikr@gmail.com>
2022-03-20 16:18:32 +0100

    Fix  text style in the main section's footer

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full h-full bg-white h-14">
     <div class="flex-auto space-x-4 p-3">
-      <p class="float-left pl-2">© 2022 All Rights Reserved.</p>
-      <p class="float-right pr-2">Email: bashar.altaleb92@gmail.com</p>
+      <p class="float-left pl-2 text-sm text-slate-600">© 2022 All Rights Reserved.</p>
+      <p class="float-right pr-2 text-sm text-slate-600">Email: bashar.altaleb92@gmail.com</p>
     </div>
   </div>
 </template>
