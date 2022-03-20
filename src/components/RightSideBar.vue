cb87a467e0ab80df55d59f2fe9f2edb910927b5e
Bashar <bashikr@gmail.com>
2022-03-20 15:38:53 +0100

    Add hamburger menu for the right sidebar

--- src/components/RightSideBar.vue
+++ src/components/RightSideBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" w-full h-full bg-white">
-    <p>-------------</p>
+  <div class=" w-full h-full bg-zinc-100">
+    <fa class="p-8  fa-lg bg-white" icon="bars" />
   </div>
 </template>
 
