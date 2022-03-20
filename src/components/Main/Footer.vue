e033d3ad925df72de7768b73a4d4e172dc2e7c3f
Bashar <bashikr@gmail.com>
2022-03-20 15:41:29 +0100

    Add header and footer to the main section

---
+++ src/components/Main/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full h-full bg-white h-14">
+    <div class="flex-auto space-x-4 p-3">
+      <p class="float-left pl-2">© 2022 All Rights Reserved.</p>
+      <p class="float-right pr-2">Email: bashar.altaleb92@gmail.com</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/Main/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-96 relative top-0 right-0">
+    <div class="bg-white mx-12 pt-6 mt-7 shadow-md rounded-t-sm"></div>
+    <img
+      class="object-cover h-80 w-full shadow-lg top-0 relative right-0"
+      src="../../assets/cover.jpg"
+      alt=""
+    />
+    <img
+      class="w-[14.6rem] top-0 absolute right-0"
+      src="../../assets/bashar-cover.png"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
